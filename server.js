58e939b61571999e238ab90ddf9f93a9a06c9bfa
ALIM664 <rymkulovalim645@gmail.com>
2026-09-12 16:22:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
-const SECRET = process.env.JWT_SECRET;
+const SECRET = "super_secret_key_change_this";
 
 app.use(cors());
 app.use(express.json());
